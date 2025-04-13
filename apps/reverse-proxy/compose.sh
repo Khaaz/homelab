24a3a5468df6c82d4828c498eb6c4b062c6655c9
Khaaz <khaaz.dev@gmail.com>
2025-04-13 20:18:53 +0200

    feat(reverse-proxy): add pre-support for new apps and make it work whether apps are enabled or not

--- apps/reverse-proxy/compose.sh
+++ apps/reverse-proxy/compose.sh
@@ -10,16 +10,32 @@ fi
 
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
+add_env_file() {
+  [ -f "$1" ] && echo "--env-file $1"
+}
+
 # compose envs
 DEFAULT_ENV_FILE="$SCRIPT_DIR/src/config/default.env"
 OVERRIDE_ENV_FILE="$SCRIPT_DIR/src/config/.env"
 NETWORKING_ENV_FILE="$SCRIPT_DIR/src/config/networking.default.env"
 NETWORKING_OVERRIDE_ENV_FILE="$SCRIPT_DIR/src/config/networking.env"
 
+HA_NETWORKING_ENV_FILE="$SCRIPT_DIR/../home-automation/src/config/networking.default.env"
+HA_NETWORKING_OVERRIDE_ENV_FILE="$SCRIPT_DIR/../home-automation/src/config/networking.env"
+IM_NETWORKING_ENV_FILE="$SCRIPT_DIR/../immich/src/config/networking.default.env"
+IM_NETWORKING_OVERRIDE_ENV_FILE="$SCRIPT_DIR/../immich/src/config/networking.env"
+
 MM_NETWORKING_ENV_FILE="$SCRIPT_DIR/../media-management/src/config/networking.default.env"
 MM_NETWORKING_OVERRIDE_ENV_FILE="$SCRIPT_DIR/../media-management/src/config/networking.env"
 MS_NETWORKING_ENV_FILE="$SCRIPT_DIR/../media-server/src/config/networking.default.env"
 MS_NETWORKING_OVERRIDE_ENV_FILE="$SCRIPT_DIR/../media-server/src/config/networking.env"
+
+NC_NETWORKING_ENV_FILE="$SCRIPT_DIR/../nextcloud/src/config/networking.default.env"
+NC_NETWORKING_OVERRIDE_ENV_FILE="$SCRIPT_DIR/../nextcloud/src/config/networking.env"
+VW_NETWORKING_ENV_FILE="$SCRIPT_DIR/../vaultwarden/src/config/networking.default.env"
+VW_NETWORKING_OVERRIDE_ENV_FILE="$SCRIPT_DIR/../vaultwarden/src/config/networking.env"
+VSC_NETWORKING_ENV_FILE="$SCRIPT_DIR/../vscode-server/src/config/networking.default.env"
+VSC_NETWORKING_OVERRIDE_ENV_FILE="$SCRIPT_DIR/../vscode-server/src/config/networking.env"
 
 # source
 COMPOSE_FILE="$SCRIPT_DIR/src/docker-compose.yaml"
@@ -31,7 +47,12 @@ echo "Root: $SCRIPT_DIR"
 docker compose -f $COMPOSE_FILE \
     --env-file $DEFAULT_ENV_FILE \
     --env-file $NETWORKING_ENV_FILE --env-file $NETWORKING_OVERRIDE_ENV_FILE \
-    --env-file $MM_NETWORKING_ENV_FILE --env-file $MM_NETWORKING_OVERRIDE_ENV_FILE \
-    --env-file $MS_NETWORKING_ENV_FILE --env-file $MS_NETWORKING_OVERRIDE_ENV_FILE \
+    --env-file $HA_NETWORKING_ENV_FILE $(add_env_file "$HA_NETWORKING_OVERRIDE_ENV_FILE") \
+    --env-file $IM_NETWORKING_ENV_FILE $(add_env_file "$IM_NETWORKING_OVERRIDE_ENV_FILE") \
+    --env-file $MM_NETWORKING_ENV_FILE $(add_env_file "$MM_NETWORKING_OVERRIDE_ENV_FILE") \
+    --env-file $MS_NETWORKING_ENV_FILE $(add_env_file "$MS_NETWORKING_OVERRIDE_ENV_FILE") \
+    --env-file $NC_NETWORKING_ENV_FILE $(add_env_file "$NC_NETWORKING_OVERRIDE_ENV_FILE") \
+    --env-file $VW_NETWORKING_ENV_FILE $(add_env_file "$VW_NETWORKING_OVERRIDE_ENV_FILE") \
+    --env-file $VSC_NETWORKING_ENV_FILE $(add_env_file "$VSC_NETWORKING_OVERRIDE_ENV_FILE") \
     --env-file $OVERRIDE_ENV_FILE $COMMAND \
     "$@"
