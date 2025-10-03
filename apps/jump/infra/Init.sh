e3ff91b693ad6ec4504b8590d4f03ec4d681e636
Khaaz <khaaz.dev@gmail.com>
2025-10-04 00:07:02 +0200

    feat(apps/jump): adjust VM init

--- apps/jump/infra/Init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Install Python and pip
-apk add --no-cache python3 py3-pip
-
-# Install ansible
-apk add ansible
---
+++ apps/jump/infra/init.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Install base packages for jump VM
+apk add --no-cache bash net-tools iputils iproute2 bind-tools nmap
+
+# Install generate env packages (argon2)
+apk add --no-cache argon2
+
+# Install Python and pip
+apk add --no-cache python3 py3-pip
+
+# Install ansible
+apk add ansible
