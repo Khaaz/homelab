da68d76f9985955f7527e64e70418415e21dd8f7
Khaaz <khaaz.dev@gmail.com>
2025-10-15 00:41:06 +0200

    feat(environment,apps/jump): allow passing refresh config level

--- environment/4-2.setup_vm.sh
+++ environment/4-2.setup_vm.sh
@@ -10,28 +10,33 @@ SCRIPT_DIR=$(get_script_dir)
 
 ## Usage
 usage() {
-	echo "Usage: $0 [--auto] <vm>"
-	echo "--auto mode will use sensible defaults"
-	exit 1
+    echo "Usage: $0 [--auto] [--refresh-config <none|generated|full>] <vm>"
+    echo "--auto mode will use sensible defaults (implies --refresh-config full)"
+    exit 1
 }
 
 ## Input verification
 AUTO=false
+REFRESH_CONFIG="full"
 VM_NAME=""
 while [ $# -gt 0 ]; do
-	case "$1" in
-		--auto)
-			AUTO=true
-			shift
-			;;
-		--help)
-			usage
-			;;
-		*)
-			VM_NAME="$1"
-			shift
-			;;
-	esac
+    case "$1" in
+        --auto)
+            AUTO=true
+            shift
+            ;;
+        --refresh-config|-rc)
+            REFRESH_CONFIG=$2
+            shift 2
+            ;;
+        --help)
+            usage
+            ;;
+        *)
+            VM_NAME="$1"
+            shift
+            ;;
+    esac
 done
 
 if [ -z  "$VM_NAME" ]; then
@@ -47,16 +52,18 @@ fi
 #
 AUTO_ARGS=""
 if [ "$AUTO" = true ]; then
-	AUTO_ARGS="--auto"
+    AUTO_ARGS="--auto"
+    # Auto mode forces full refresh
+    REFRESH_CONFIG="full"
 fi
 
 if [ "$VM_NAME" = "jump" ]; then
 	$SCRIPT_DIR/4-1.setup_vm_jump.sh $AUTO_ARGS
 else
-	# Execute command directely via jump / resolve vm name via dns
+    # Execute command directely via jump / resolve vm name via dns
 	ssh -i $SCRIPT_DIR/../config/ssh/jump/admin_key \
 		-o StrictHostKeyChecking=no \
 		-p 2222 \
 		admin@192.168.1.200 \
-		"./homelab/apps/jump/ansible_vm.sh $VM_NAME"
+        "./homelab/apps/jump/ansible_vm.sh --refresh-config $REFRESH_CONFIG $VM_NAME"
 fi
