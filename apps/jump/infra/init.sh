b7fea9a8f74c8c39debc5bfa63d45fd59af9324b
Khaaz <khaaz.dev@gmail.com>
2025-11-11 13:53:42 +0100

    feat(environment,apps/jump): bcrypt lib

--- apps/jump/infra/init.sh
+++ apps/jump/infra/init.sh
@@ -3,8 +3,8 @@
 # Install base packages for jump VM
 apk add --no-cache bash net-tools iputils iproute2 bind-tools nmap
 
-# Install generate env packages (argon2)
-apk add --no-cache argon2
+# Install generate env packages (passwords)
+apk add --no-cache argon2 apache2-utils
 
 # Install Python and pip
 apk add --no-cache python3 py3-pip
