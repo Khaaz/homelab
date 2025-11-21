8004327913667ea3f7aef8ea8aa0832ed49e64eb
Khaaz <khaaz.dev@gmail.com>
2025-11-21 21:38:40 +0100

    feat(apps/reverse-proxy): support *.sandbox.<internal.domain>

--- apps/reverse-proxy/apps/ca/scripts/entrypoint.sh
+++ apps/reverse-proxy/apps/ca/scripts/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ## Generate certs
-/app/minica/minica -ca-key root.key -ca-cert root.crt --domains $DOMAIN,*.$DOMAIN -common-name "$ROOT_CN"
+/app/minica/minica -ca-key root.key -ca-cert root.crt --domains $DOMAIN,*.$DOMAIN,*.sandbox.$DOMAIN -common-name "$ROOT_CN"
 
 ## root
 # root cert
