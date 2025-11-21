cca537fe6a95f1c59e80f957f4e14f2ddf4b0ada
Khaaz <khaaz.dev@gmail.com>
2025-11-21 12:01:24 +0100

    feat(apps/media-server): add transcode folder

--- apps/media-server/infra/init.sh
+++ apps/media-server/infra/init.sh
@@ -6,6 +6,8 @@ mkdir -p /mnt/data/media/movies
 mkdir -p /mnt/data/media/series
 mkdir -p /mnt/data/media/unsorted
 
+mkdir -p /mnt/data/transcode
+
 # Change permissions
 chown -R nobody:nogroup /mnt/data
 chmod -R 2777 /mnt/data
